871b6ec5c526e6fa02374c888abffd0b951f637a
Hisham Bhatti <hisham.bhatti03@gmail.com>
2025-09-04 12:48:13 -0700

    feat: backend deleted images after generating meme

--- backend/app.py
+++ backend/app.py
@@ -65,11 +65,17 @@ def upload_image():
 # TODO figure out how to pass the image into this request
 def generate_cap():
     img_path = request.args.get('src')
-
-    mycaption = model.generate_caption(img_path=os.path.join(UPLOAD_DIR, img_path))
+    
+    # check if img_path is a valid image that was uploaded
+    if (not os.path.exists(os.path.join(UPLOAD_DIR, img_path))):
+        return "image path invalid", 404
 
     # generate caption here
+    mycaption = model.generate_caption(img_path=os.path.join(UPLOAD_DIR, img_path))
+
+    # delete the image after a caption has been generated from it
+    os.remove(os.path.join(UPLOAD_DIR, img_path))
+
     my_response = make_response(jsonify({"caption": mycaption}), 200)
-    # my_response.headers['Access-Control-Allow-Origin'] = '*'
 
     return my_response
